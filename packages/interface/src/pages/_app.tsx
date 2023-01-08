039c4e5f0e6dcceef5b27491954e759adbfa3206
yaaman18 <gucchiyama8551@hotmail.co.jp>
2023-01-09 01:55:15 +0900

    master

--- packages/interface/src/pages/_app.tsx
+++ packages/interface/src/pages/_app.tsx
@@ -1,6 +1,38 @@
-import '../styles/globals.css'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import '@rainbow-me/rainbowkit/styles.css'
 import type { AppProps } from 'next/app'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { mainnet, polygon } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import '../styles/globals.css'
+
+if (!process.env.NEXT_PUBLIC_ALCHEMY_KEY) {
+  throw new Error('Please set your Alchemy ID in your .env file')
+}
+
+const { chains, provider } = configureChains(
+  [polygon, mainnet],
+  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY }), publicProvider()],
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'Contract template',
+  chains,
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
